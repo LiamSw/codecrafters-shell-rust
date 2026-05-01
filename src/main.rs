73bdf862bc012668775e8b2f94b933b2df355409
lsawadogo <liam.sawadogo@student.uclouvain.be>
2026-05-01 21:58:47 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,11 @@ fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
 
-    let _ = String::new();
-    io::stdin().read_line(&mut input);
-
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input);
+        .expect("Failed to read line");
+        
     print!("{input}: command not found");
 
     io::stdout().flush().unwrap();
