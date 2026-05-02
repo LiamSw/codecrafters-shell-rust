084d366c800af096f9664559c0b68141cfafbe04
lsawadogo <liam.sawadogo@student.uclouvain.be>
2026-05-02 20:52:19 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::env;
+use std::fs;
+use is_executable::is_executable;
 
 fn main() {
     loop {
@@ -26,7 +29,27 @@ fn main() {
                 if recognized_com.contains(cmd_type) {
                     println!("{cmd_type} is a shell builtin");
                 } else {
-                    println!("{cmd_type}: not found");
+                    let key = "PATH";
+                    match env::var_os(key) {
+                        Some(paths) => {
+                            for path in env::split_paths(&paths) {
+                                let test_path = path.join(cmd_type);
+
+                                match fs::exists(test_path) {
+                                    Ok(true) => {
+                                        if is_executable(&test_path) {
+                                            println!("{cmd_type} is {}", test_path.display());}
+                                            return;
+                                        else {continue;}
+                                    }
+                                    Ok(false) => continue,
+                                    Err(e) => println!("Error checking path: {}", e),
+                                }
+                            }
+                            println!("{cmd_type}: not found");
+                        }
+                        None => println!("{key} is not defined in the environment.")
+                    }
                 }
             }
             _ => println!("{command}: command not found"),
